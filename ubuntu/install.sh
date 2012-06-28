40782ead94b29417540da5a26f8ccbb1ee4d43db
Haochi Chen <haochi.chen@gmail.com>
2012-06-28 06:01:49 +0400

    put all the languages into one command

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -11,10 +11,10 @@ apt-get -y install git-core mercurial
 apt-get -y install curl build-essential openssl libssl-dev
 
 #languages
-apt-get -y install nodejs npm
-apt-get -y install golang-stable
-apt-get -y install ruby1.9.1-full
-apt-get -y install libyaml-ruby libzlib-ruby rubygems
-apt-get -y install mono-complete
-apt-get -y install python3
-apt-get -y install ghc6 ghc6-prof ghc6-doc
+apt-get -y install nodejs npm \
+                   golang-stable \
+                   ruby1.9.1-full \
+                   libyaml-ruby libzlib-ruby rubygems \
+                   mono-complete \
+                   python3 \
+                   ghc6 ghc6-prof ghc6-doc
