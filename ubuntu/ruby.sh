083c48bfe35085302a47cee60248da760ae37c54
Haochi Chen <haochi.chen@gmail.com>
2012-06-30 08:02:35 +0400

    use rvm instead. sigh

--- ubuntu/ruby.sh
+++ ubuntu/ruby.sh
@@ -1,4 +1,1 @@
-git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
-mkdir -p ~/.rbenv/plugins
-git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins
-
+curl -L https://get.rvm.io | bash -s stable
